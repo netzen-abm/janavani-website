64caf54c123aa184b403dc73c7036514c3c0aff3
NetZen <mycoach808@proton.me>
2026-08-11 21:38:38 +0530

    Create gtranslation.js

---
+++ gtranslation.js
@@ -0,0 +1,34 @@
+/*
+ * Janavani Website Translation
+ *
+ * Temporary multilingual layer.
+ * Primary language: English.
+ *
+ * Languages:
+ * English
+ * Malayalam
+ * Hindi
+ * Marathi
+ * Tamil
+ * Kannada
+ * Telugu
+ *
+ * Important:
+ * Machine translation is provided for accessibility.
+ * Important legal and constitutional material should be
+ * human-reviewed before being treated as authoritative.
+ */
+
+(function () {
+
+    window.gtranslateSettings = {
+        default_language: "en",
+        native_language_names: true,
+        detect_browser_language: true,
+        wrapper_selector: ".gtranslate_wrapper",
+        switcher_horizontal_position: "right",
+        switcher_vertical_position: "top",
+        flag_style: "3d"
+    };
+
+})();
