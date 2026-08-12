59a5fe58cbcaecb24ee2a33833f4cb0eb999892d
NetZen <mycoach808@proton.me>
2026-08-12 11:40:55 +0530

    Update gtranslation.js

--- gtranslation.js
+++ gtranslation.js
@@ -21,18 +21,12 @@
 
 (function () {
 
-    window.gtranslateSettings = {
-        default_language: "en",
-        native_language_names: true,
-        detect_browser_language: true,
-        wrapper_selector: ".gtranslate_wrapper",
-        switcher_horizontal_position: "right",
-        switcher_vertical_position: "top",
-        flag_style: "3d"
-    };
+    <div class="gtranslate_wrapper"></div>
+<script>window.gtranslateSettings = {"default_language":"en","native_language_names":true,"detect_browser_language":true,"languages":["en","ml","hi","kn","te","ta","mr","bn","gu","pa","sd","ur","ne"],"wrapper_selector":".gtranslate_wrapper","switcher_horizontal_position":"right","switcher_vertical_position":"top","flag_style":"3d"}</script>
+<script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
+
+
+;
 
 })();
 
-<div class="gtranslate_wrapper"></div>
-<script>window.gtranslateSettings = {"default_language":"en","native_language_names":true,"detect_browser_language":true,"languages":["en","ml","hi","kn","te","ta","mr","bn","gu","pa","sd","ur","ne"],"wrapper_selector":".gtranslate_wrapper","switcher_horizontal_position":"right","switcher_vertical_position":"top","flag_style":"3d"}</script>
-<script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
